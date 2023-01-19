6c953d4d532eb403191596d23956186f2f1fcb51
MAEJOK <maej@pm.me>
2023-01-18 20:13:48 -0500

    init

---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'Postal Code Map and Minimap'
+version '1.3'
+repository 'https://forum.cfx.re/t/free-release-postal-code-map-minimap-fixed/4882127'
+
+client_script 'client.lua'
